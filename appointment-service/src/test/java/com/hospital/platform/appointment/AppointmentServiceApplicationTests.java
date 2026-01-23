b5e7e57d8d9a715b1a3f2c7dbdd024d6eb4acbe7
Lucky Singh <lucky.singh8555@gmail.com>
2026-01-23 23:11:01 +0530

    initial commit

---
+++ appointment-service/src/test/java/com/hospital/platform/appointment/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospital.platform.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
